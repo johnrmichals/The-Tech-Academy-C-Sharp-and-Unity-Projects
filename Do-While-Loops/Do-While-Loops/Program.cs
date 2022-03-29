d0dc57332445005c806e5db476d66ccae89fc90d
johnrmichals <johnrmichals@gmail.com>
2022-03-29 13:58:41 -0700

    Loop Assignment

---
+++ Do-While-Loops/Do-While-Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Do_While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Let's count to 5!");
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 5);
+            Console.WriteLine(i);
+            
+            string answer = "";
+            while (answer != "gesundheit")
+            {   
+                Console.WriteLine("Achoo! (say gesundheit!)");
+                answer = Console.ReadLine();
+            }
+            
+        }        
+    }
+}
