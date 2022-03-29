9e0b21f5697a7bb6a1c63cf9d544cd31ecad8f5c
johnrmichals <johnrmichals@gmail.com>
2022-03-28 17:23:12 -0700

    updated program

--- PackageDimensions/PackageDimensions/Program.cs
+++ PackageDimensions/PackageDimensions/Program.cs
@@ -16,26 +16,34 @@ namespace PackageDimensions
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("How much does your package weigh?");
             weight = Convert.ToInt16(Console.ReadLine());
-                if (weight > 50) { 
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-                Console.ReadLine();
-                System.Environment.Exit(0);
-                }
-            Console.WriteLine("What is the height of your package?");
-            height = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("What is the width of your package?");
-            width = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("What is the length of your package?");
-            length = Convert.ToInt16(Console.ReadLine());
-                if (weight > 50 || height > 50 || length > 50 ) {
+            if (weight > 50)
+            {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
                 Console.ReadLine();
                 System.Environment.Exit(0);
             }
-            total = ((width * height * length) * weight ) / 100;
-            Console.WriteLine("Your esitmated total for this package is $" + total);
-            Console.WriteLine("Thank you!");
-            Console.ReadLine();
+            else
+            { 
+                Console.WriteLine("What is the height of your package?");
+                height = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("What is the width of your package?");
+                width = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("What is the length of your package?");
+                length = Convert.ToInt16(Console.ReadLine());
+                if ((width + height + length) > 50)
+                {
+                    Console.WriteLine("Package too large to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    total = ((width * height * length) * weight) / 100;
+                    Console.WriteLine("Your esitmated total for this package is $" + total);
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
