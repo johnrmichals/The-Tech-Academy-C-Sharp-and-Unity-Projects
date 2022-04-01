cbf67127aeef5362295bba1133cb7cd405b3bd60
johnrmichals <johnrmichals@gmail.com>
2022-04-01 16:58:31 -0700

    try-catch divider program

---
+++ divider/divider/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace divider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divider = 0;
+            var numbers = new List<int>() { 2, 8, 29, 134, 77, 12, 44, 1 };
+            try
+            {
+                Console.WriteLine("Pick a number to divide my numbers by");
+                divider = Convert.ToInt32(Console.ReadLine());
+                foreach (int x in numbers)
+                {
+                    Console.WriteLine(x / divider);
+                }
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
