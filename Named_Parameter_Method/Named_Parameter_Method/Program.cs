a4c2a08b3277c26a52882fb394f5495a84f270c7
johnrmichals <johnrmichals@gmail.com>
2022-04-10 12:33:28 -0700

    Named Param Proj

---
+++ Named_Parameter_Method/Named_Parameter_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Named_Parameter_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gimme two numbers!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            twoNums TwoNumbers = new twoNums();
+            TwoNumbers.TwoNumbers(a, b);
+            TwoNumbers.TwoNumbers(x: a, y: b);
+        }
+    }
+}
---
+++ Named_Parameter_Method/Named_Parameter_Method/twoNums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Named_Parameter_Method
+{
+    class twoNums
+    {
+        public void TwoNumbers(int x, int y)
+        {
+            x += 10;
+            Console.WriteLine(y);
+        }
+    }
+}
