d1240c930948eb39f5a6583dabe26d0b0e0af84a
johnrmichals <johnrmichals@gmail.com>
2022-04-10 11:22:18 -0700

    method conversion proj

---
+++ Method_Submission/Method_Submission/AddEmUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Submission
+{
+    class AddEmUp
+    {
+        public int AddEm(int x = 0, int y = 0)
+        {            
+                return x + y;            
+        }
+    }
+}
---
+++ Method_Submission/Method_Submission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Method_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            Console.WriteLine("Gimme one or two numbers (second number can be empty) and I'll add 'em up!");
+            var x = Console.ReadLine();                      
+            var z = Console.ReadLine();
+            AddEmUp MathObj = new AddEmUp();
+            if (z == "")
+            {
+                y = 0;
+            }
+            else
+                y = Convert.ToInt32(z);
+            Console.WriteLine(MathObj.AddEm(Convert.ToInt32(x), y));
+            Console.ReadLine();
+
+        }
+    }
+}
