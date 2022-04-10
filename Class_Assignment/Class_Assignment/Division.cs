cc77a7ac65aa687dacd535d02c137b4e7859ac28
johnrmichals <johnrmichals@gmail.com>
2022-04-10 13:09:20 -0700

    Classes Proj

---
+++ Class_Assignment/Class_Assignment/Division.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Assignment
+{
+    class Division
+    {
+        public void DivideByTwo(int x)
+        {
+            Console.WriteLine(x / 2);
+        }
+        public int TimesThree(int x) //method with output parameters
+        {
+            return x * 3;
+        }
+        public float TimesThree(float x) //overloading a method
+        {
+            return x / 3;
+        }
+    }
+}
---
+++ Class_Assignment/Class_Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Class_Assignment
+{
+    static class Program //program class declared static 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number and I'll divide it in two!"); 
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Division division = new Division(); //instantiates the method
+            division.DivideByTwo(y); //calls the method with user input
+        }
+    }
+}
