f5db2099ec51edf0ff88300a752d4543dc677d08
johnrmichals <johnrmichals@gmail.com>
2022-04-13 12:47:17 -0700

    Polymorphism proj

--- Polymorphism/Methods_and_Objects/Employee.cs
+++ Polymorphism/Methods_and_Objects/Employee.cs
@@ -4,11 +4,17 @@ using System.Text;
 
 namespace Methods_and_Objects
 {
-    class Employee : Person
+    class Employee : Person, IQuittable
     {
         public override void SayName()
         {
             Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Press Enter to Quit");
+            Console.ReadLine();
+            System.Environment.Exit(1);
         }
     }
 }
---
+++ Polymorphism/Methods_and_Objects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            person.SayName();
+            person.Quit();
+        }
+    }
+}
