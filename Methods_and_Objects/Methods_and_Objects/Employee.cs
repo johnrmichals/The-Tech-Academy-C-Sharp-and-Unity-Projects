99aa912a3a9510d9202d1574008c7ae59821a220
johnrmichals <johnrmichals@gmail.com>
2022-04-12 20:10:01 -0700

    Methods/Objects Proj

---
+++ Methods_and_Objects/Methods_and_Objects/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_and_Objects
+{
+    class Employee : Person 
+    {        
+        public int Id; 
+    }
+}
---
+++ Methods_and_Objects/Methods_and_Objects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Methods_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+
+            person.SayName();
+        }
+    }
+}
