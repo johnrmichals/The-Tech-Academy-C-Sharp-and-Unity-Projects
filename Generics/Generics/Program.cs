b8e72b8ff57d0089e6ad472d3b9e10fe9e27853a
johnrmichals <johnrmichals@gmail.com>
2022-04-20 15:18:57 -0700

    Generic Types

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class program
+    {  
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "Joe", "Mac", "Sam"};
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>() { 4, 6, 8 };
+
+            for (int i = 0; i < employee.Things.Count; i++)
+            {
+                Console.WriteLine(employee.Things[i] + " " + employee2.Things[i]);
+            }
+        }        
+    }
+}
